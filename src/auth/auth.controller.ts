f16b0c4991318f377931259888f83463d7d6a5e2
SEN-SENSATION <prawich.th@gmail.com>
2022-04-22 01:00:10 +0700

    TodoAPI | v1.0 Development (0.1) - 22/4/2022 00:59

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,37 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  Headers,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Ip,
+  Logger,
+  Post,
+  Req,
+} from "@nestjs/common";
+import { Request } from "express";
 
-@Controller()
-export class auth {
+@Controller("auth")
+export class AuthController {
+  @Get()
+  async getUser(
+    @Req() req: Request,
+    @Ip() ip: string,
+    @Headers("authorization") auth,
+  ): Promise<object> {
+    const token = req.headers.authorization;
+    Logger.log(`token are: ${token} and ${auth}`);
+    throw new HttpException(
+      "You are forbidden from this endpoint, sorry for the inconvinent",
+      HttpStatus.FORBIDDEN,
+    );
+    return {
+      user: "This is a user",
+    };
+  }
+
   @Post()
   async login(): Promise<object> {
     return {};
